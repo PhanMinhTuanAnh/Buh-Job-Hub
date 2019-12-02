0b4d76a5312b76b4df657a2a965570ec86106e45
Tuan Anh <anhphanaabb@gmail.com>
2019-12-03 04:32:34 +0700

    create react-redux's file structure

---
+++ buh-job-hub/src/components/_Main/Main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path='/' component={}/>
+            </Switch>
+        )
+    }
+}
--- buh-job-hub/src/index.js
+++ buh-job-hub/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/_App/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux';
+import appReducer from './react-redux/index_reducer.js'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const composeEnhancers = composeWithDevTools({
+});
+const store = createStore(
+    appReducer,
+    composeEnhancers(
+        applyMiddleware(thunk),
+    )
+);
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
