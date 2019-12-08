0693f1dae4a0482e54dec4e05b4cd387ba70280a
Tuan Anh <anhphanaabb@gmail.com>
2019-12-08 12:45:06 +0700

    add Link and add [Home] Component

--- buh-job-hub/src/components/_Main/Main.jsx
+++ buh-job-hub/src/components/_Main/Main.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Home from '../Home/Home'
 import JobPage from '../Pages/JobPage/JobPage'
 import ResumePage from '../Pages/ResumePage/ResumePage'
 import Contact from '../Pages/Contact/Contact'
@@ -7,11 +8,12 @@ import Account from '../Account/Account'
 import BrowseJob from '../BrowseListings/BrowseJob/BrowseJob'
 import BrowseResumes from '../BrowseListings/BrowseResumes/BrowseResumes'
 import BrowseCategories from '../BrowseListings/BrowseCategories/BrowseCategories'
+
 export default class Main extends Component {
     render() {
         return (
             <Switch>
-                {/* <Route exact path='/' component={Home}/> */}
+                <Route exact path='/' component={Home}/>
                 <Route exact path='/job-page' component={JobPage}/>
                 <Route exact path='/account' component={Account}/>
                 <Route exact path='/resume-page' component={ResumePage}/>
